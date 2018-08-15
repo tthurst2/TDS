2e77dc01baa6ded642d3595ba56d3006bb5314eb
Tyler Thurston <tylerthurston1@gmail.com>
2018-08-15 18:52:06 -0400

    Added shotgun behavior, PlayerShotgun script

---
+++ TDS/Assets/Scripts/PlayerShotgun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShotgun : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+    public float fireDelay = 0.25f;
+    float cooldownTimer = 0;
+    int bulletLayer;
+    float tiltAngle = 15.0f;
+    private void Start() {
+        bulletLayer = 11; //player bullet layer
+    }
+
+    void Update() {
+        cooldownTimer -= Time.deltaTime;
+
+
+        if (Input.GetButton("Fire1") && cooldownTimer <= 0) {
+            //Fire weapon
+            cooldownTimer = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletOffset;
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+            //two side bullets (shotgun effect)
+            GameObject bulletGO2 = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation * Quaternion.Euler(0, 0, tiltAngle));
+            GameObject bulletGO3 = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation * Quaternion.Euler(0, 0, -tiltAngle));
+            bulletGO.layer = bulletLayer;
+        }
+    }
+}
