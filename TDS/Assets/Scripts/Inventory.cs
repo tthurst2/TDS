fe3a72740d6417c0e25ccd319126f8e03654e1a1
tthurst2 <user.password>
2018-08-23 16:22:04 -0400

    Added singleton inventory region

--- TDS/Assets/Scripts/Inventory.cs
+++ TDS/Assets/Scripts/Inventory.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class Inventory : MonoBehaviour {
 
 
+    #region Singleton
+    public static Inventory instance;
+
+    void Awake () {
+
+        if (instance != null) {
+            Debug.LogWarning("More than one inventory found");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+    //Weapon list
     public List<GameObject> weapons = new List<GameObject>();
 
     public void Add (GameObject weapon) {
