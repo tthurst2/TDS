b8fcfda72dd991b31ce8a4ed88ce302491e8f17e
tthurst2 <user.password>
2018-08-23 16:18:42 -0400

    Inventory with list of weapons added to GameManager

--- TDS/Assets/Scripts/Inventory.cs
+++ TDS/Assets/Scripts/Inventory.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public List<GameObject> weapons = new List<GameObject>();
+
+    public void Add (GameObject weapon) {
+        weapons.Add(weapon);
+    }
+
+    public void Remove(GameObject weapon) {
+        weapons.Remove(weapon);
+    }
+
 }
