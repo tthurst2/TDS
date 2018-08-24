7a3fa5a05508a52e809e7bd5af6727ef06d60b6d
Tyler Thurston <tylerthurston1@gmail.com>
2018-08-24 18:22:49 -0400

    Added beginnings of weapon UI

--- TDS/Assets/BombExplosion.cs
+++ TDS/Assets/BombExplosion.cs
@@ -19,9 +19,12 @@ public class BombExplosion : MonoBehaviour {
 
     private void OnDestroy() {
         //create big explosion nova
-        for(int i = 0; i < (int)novaBullets; i++) {
-            Instantiate(Inventory.instance.bullets[0], transform.position, transform.rotation * Quaternion.Euler(0, 0, i * (360f/novaBullets)));
-        }
         
+            for (int i = 0; i < (int)novaBullets; i++) {
+            if(Inventory.instance != null) {
+                Instantiate(Inventory.instance.bullets[0], transform.position, transform.rotation * Quaternion.Euler(0, 0, i * (360f / novaBullets)));
+                }
+            }   
+
     }
 }
