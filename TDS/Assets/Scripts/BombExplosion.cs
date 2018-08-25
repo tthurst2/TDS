205f7472fcd36f90a88b97c5acf555bceb0df8f6
Tyler Thurston <tylerthurston1@gmail.com>
2018-08-25 11:06:55 -0400

    Fixed bomb ondestroy instantiation bug

--- TDS/Assets/Scripts/BombExplosion.cs
+++ TDS/Assets/Scripts/BombExplosion.cs
@@ -6,25 +6,20 @@ public class BombExplosion : MonoBehaviour {
 
     //number of bullets we want in explosive nova
     public float novaBullets = 45f;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool isQuitting = false;
+    private void OnApplicationQuit() {
+        isQuitting = true;
+    }
 
     private void OnDestroy() {
         //create big explosion nova
-        
-            for (int i = 0; i < (int)novaBullets; i++) {
-            if(Inventory.instance != null) {
-                Instantiate(Inventory.instance.bullets[0], transform.position, transform.rotation * Quaternion.Euler(0, 0, i * (360f / novaBullets)));
-                }
-            }   
 
+        if (!isQuitting) {
+            for (int i = 0; i < (int)novaBullets; i++) {
+                if (Inventory.instance != null) {
+                    Instantiate(Inventory.instance.bullets[0], transform.position, transform.rotation * Quaternion.Euler(0, 0, i * (360f / novaBullets)));
+                }
+            }
+        }
     }
 }
