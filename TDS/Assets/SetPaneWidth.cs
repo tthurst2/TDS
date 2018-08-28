b81def3d58da4c0b1cf1603cf490fb710acac4c3
tthurst2 <user.password>
2018-08-28 14:50:56 -0400

    UI hinged to bottom-right corner

---
+++ TDS/Assets/SetPaneWidth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPaneWidth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2((150 * transform.childCount) - 50, 100);
+    }
+	
+}
