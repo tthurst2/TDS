7a5e499bfcad46a932ffebfd388cbfcc471db5f7
tthurst2 <user.password>
2018-08-28 15:49:44 -0400

    weaponUI now updates with keyboard controls

--- TDS/Assets/Scripts/Weapons/Manager/WeaponManager.cs
+++ TDS/Assets/Scripts/Weapons/Manager/WeaponManager.cs
@@ -5,55 +5,7 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour {
 
     Inventory inv = Inventory.instance;
-    float weaponSwapTimer = 0f;
-    public int weaponIndex;
-    void Start() {
-        LoadWeapons();
-        EquipWeapon(0);
-        //transform.GetChild(0).gameObject.SetActive(true); 
-    }
-
-    void Update() {
-        weaponSwapTimer -= Time.deltaTime;
-
-        //scroll wheel through entire weapon list
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
-            if (weaponIndex >= transform.childCount - 1)
-                EquipWeapon(0);
-            else
-                EquipWeapon(weaponIndex + 1);
-        }
-
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
-            if (weaponIndex <= 0)
-                EquipWeapon(transform.childCount - 1);
-            else
-                EquipWeapon(weaponIndex - 1);
-        }
-
-
-        //number keys for first five elements
-        if (Input.GetKeyDown(KeyCode.Alpha1) && weaponIndex != 0 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 1) {
-            EquipWeapon(0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2) && weaponIndex != 1 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 2) {
-            EquipWeapon(1);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3) && weaponIndex != 2 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 3) {
-            EquipWeapon(2);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && weaponIndex != 3 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 4) {
-            EquipWeapon(3);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha5) && weaponIndex != 4 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 5) {
-            EquipWeapon(4);
-        }
-
-        
-    }
+    public SelectedWeaponUI swUI;
 
     public void LoadWeapons() {
         for(int i = 0; i < inv.weaponList.Count; i++) {
@@ -66,14 +18,17 @@ public class WeaponManager : MonoBehaviour {
 
     public void EquipWeapon(int index) {
         //Debug.Log("Equip Weapon: " + transform.childCount);
-        weaponIndex = index;
-        int i = 0;
-        foreach (Transform weapon in transform) {
-            if (i == index)
-                weapon.gameObject.SetActive(true);
-            else 
-                weapon.gameObject.SetActive(false);
-                i++;            
+        //Debug.Log("Manager index :" + index);
+
+        for (int i = 0; i <= transform.childCount - 1; i++) {
+            if(i == index) {
+                transform.GetChild(i).gameObject.SetActive(true);
+                swUI.UpdateSelectedUI(i);
+            }
+            else {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
         }
+
     }
 }
---
+++ TDS/Assets/WeaponController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+    Inventory inv = Inventory.instance;
+    public WeaponManager wm;
+    float weaponSwapTimer = 0f;
+    public int weaponIndex;
+    void Start() {
+        wm.LoadWeapons();
+        wm.EquipWeapon(0);
+    }
+
+    void Update() {
+        weaponSwapTimer -= Time.deltaTime;
+
+        //scroll wheel through entire weapon list
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+            
+            if (weaponIndex >= transform.childCount - 1) {
+                weaponIndex = 0;
+                wm.EquipWeapon(weaponIndex);
+            }
+            else {
+                weaponIndex++;
+                wm.EquipWeapon(weaponIndex);
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+            if (weaponIndex <= 0) {
+                weaponIndex = transform.childCount - 1;
+                wm.EquipWeapon(weaponIndex);
+            }
+            else {
+                weaponIndex--;
+                wm.EquipWeapon(weaponIndex);
+            }
+        }
+
+
+        //number keys for first five elements
+        if (Input.GetKeyDown(KeyCode.Alpha1) && weaponIndex != 0 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 1) {
+            weaponIndex = 0;
+            wm.EquipWeapon(weaponIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && weaponIndex != 1 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 2) {
+            weaponIndex = 1;
+            wm.EquipWeapon(weaponIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && weaponIndex != 2 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 3) {
+            weaponIndex = 2;
+            wm.EquipWeapon(weaponIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && weaponIndex != 3 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 4) {
+            weaponIndex = 3;
+            wm.EquipWeapon(weaponIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5) && weaponIndex != 4 && weaponSwapTimer <= 0 && inv.weaponList.Count >= 5) {
+            weaponIndex = 4;
+            wm.EquipWeapon(weaponIndex);
+        }
+
+
+    }
+    
+}
