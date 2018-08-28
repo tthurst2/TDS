18a7dfee6b2626c5aecfa36aebde39b1973ffa94
tthurst2 <user.password>
2018-08-28 16:43:09 -0400

    comments added

--- TDS/Assets/Scripts/Weapons/Manager/WeaponManager.cs
+++ TDS/Assets/Scripts/Weapons/Manager/WeaponManager.cs
@@ -23,6 +23,7 @@ public class WeaponManager : MonoBehaviour {
         for (int i = 0; i <= transform.childCount - 1; i++) {
             if(i == index) {
                 transform.GetChild(i).gameObject.SetActive(true);
+                //update UI
                 swUI.UpdateSelectedUI(i);
             }
             else {
